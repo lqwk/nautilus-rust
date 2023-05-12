d3dcc31abc10f21f485543ec71399e47b69b3319
Conor Kotwasinski <91287181+conorKotwasinski@users.noreply.github.com>
2023-05-12 14:07:52 -0500

    add SAFETY comments

--- src/rust/src/parport/chardev.rs
+++ src/rust/src/parport/chardev.rs
@@ -54,7 +54,7 @@ impl<T: CharDevOps> NkCharDev<T> {
         }
     }
 
-    pub fn register(&mut self, deivce: Arc<IRQLock<T>>) -> Result<()> {
+    pub fn register(&mut self, deivce: Arc<IRQLock<T>>) -> Result {
         debug!("register device");
 
         if !self.dev.is_null() {
@@ -98,7 +98,18 @@ impl<T> Drop for NkCharDev<T> {
         }
     }
 }
-
+// SAFETY: This function is marked unsafe due to two primary safety conditions that must be upheld by the caller:
+//
+// 1. The caller must guarantee that the `state` pointer is valid, i.e., it is not null, it points to an initialized 
+//    `IRQLock<T>`, and the object it points to has not been mutated.
+//
+// 2. The caller must also ensure that the reference count of the containing `Arc` is not dropped to zero while the 
+//    returned reference exists. This is to ensure that the object the returned reference points to is not deallocated 
+//    prematurely.
+//
+// If these conditions are met, it is safe to call this function. If the `state` pointer is not valid or the 
+// reference count of the `Arc` is dropped to zero while the reference is still in use, this function can cause 
+// undefined behavior.
 unsafe fn deref_locked_state<'a, T: CharDevOps>(state: *mut c_void) -> &'a IRQLock<T> {
     // caller must guarantee `state`, and the object it points to, was not mutated
     //
@@ -108,6 +119,12 @@ unsafe fn deref_locked_state<'a, T: CharDevOps>(state: *mut c_void) -> &'a IRQLo
     unsafe { l.as_ref() }.unwrap()
 }
 
+// SAFETY: This pointer is guaranteed to be valid. The caller ensures that the `state`
+// pointer provided to the function is non-null and points to valid memory. Furthermore,
+// the `deref_locked_state` function guarantees that the object to which `state` points
+// has not been mutated. This, combined with the fact that the reference count of the 
+// containing `Arc` is not dropped to 0 while the reference returned by `deref_locked_state`
+// exists, ensures safety. 
 pub unsafe extern "C" fn status<T: CharDevOps>(state: *mut c_void) -> c_int {
     let p = unsafe { deref_locked_state::<T>(state) };
     if p.lock().is_ready() {
@@ -116,6 +133,25 @@ pub unsafe extern "C" fn status<T: CharDevOps>(state: *mut c_void) -> c_int {
         0
     }
 }
+
+// SAFETY: There are two primary safety concerns in this function.
+// 
+// 1. The `state` pointer provided by the caller is assumed to be a valid 
+//    and properly initialized pointer to a `c_void`. The `deref_locked_state` function,
+//    which is used to dereference this pointer, assumes that the pointer is valid, 
+//    and that the object it points to has not been mutated. The caller must ensure 
+//    that these conditions are met to guarantee safety.
+// 
+// 2. The `dest` pointer must point to a valid memory location where the read byte 
+//    can be safely written. The caller guarantees this, and `*dest = v` only 
+//    dereferences `dest` after this guarantee. The function does not check the 
+//    validity of `dest`, so it's crucial that the caller ensures `dest` is a valid 
+//    pointer to a `u8`.
+//
+// The caller is responsible for ensuring that the `state` and `dest` pointers 
+// provided meet these conditions, and that the `Arc` containing `state` is not 
+// dropped while the returned reference exists. If these conditions are met, 
+// the function is safe to call.
 
 pub unsafe extern "C" fn read<T: CharDevOps>(state: *mut c_void, dest: *mut u8) -> c_int {
     debug!("read!");
@@ -133,20 +169,42 @@ pub unsafe extern "C" fn read<T: CharDevOps>(state: *mut c_void, dest: *mut u8) 
         Err(_) => 0,
     }
 }
-
+// SAFETY: This function is marked as unsafe due to two key safety conditions that must be upheld by the caller:
+//
+// 1. The `state` pointer provided to this function should be a valid pointer to a `c_void`. The `deref_locked_state` 
+//    function, which is used to dereference this pointer, assumes that it is valid and that the object it points to has 
+//    not been mutated. The caller must ensure these conditions are met to guarantee safety.
+//
+// 2. The `src` pointer should point to a valid memory location containing the byte to be written. The caller 
+//    guarantees this and the function does not validate it. The function dereferences `src` after this guarantee 
+//    (`*src`), so it's crucial that the caller ensures `src` is a valid pointer to a `u8`.
+//
+// If these conditions are met, the function is safe to call. If these assumptions about the `state` and `src` 
+// pointers are not accurate, this function can lead to undefined behavior.
 pub unsafe extern "C" fn write<T: CharDevOps>(state: *mut c_void, src: *mut u8) -> c_int {
     debug!("write!");
 
     let s = unsafe { deref_locked_state::<T>(state) };
     let mut p = s.lock();
-    // caller guarantees `src` points to the correct byte to write
+    // SAFETY: Caller guarantees `src` points to the correct byte to write.
     let byte = unsafe { *src };
     match p.write(byte) {
         Ok(_) => 1,  // success
         Err(_) => 0, // failure
     }
 }
-
+// SAFETY: This function is marked as unsafe because it has two primary safety conditions that the caller must uphold:
+//
+// 1. The `_state` pointer provided by the caller should be a valid pointer to a `c_void`. Even though this function 
+//    does not dereference `_state`, the caller must ensure that this pointer is valid and that the object it points to 
+//    has not been mutated to maintain the safety guarantees of the calling context.
+//
+// 2. The `c` pointer should point to a valid memory location where the `nk_char_dev_characteristics` can be safely 
+//    written. The function uses `write_bytes(c, 0, 1)` to write to this memory, and does not validate the pointer `c`. 
+//    This means it's crucial for the caller to ensure that `c` is a valid pointer to an `nk_char_dev_characteristics`.
+//
+// If these conditions are met, the function is safe to call. If these assumptions about the `_state` and `c` 
+// pointers are not accurate, this function can lead to undefined behavior.
 pub unsafe extern "C" fn get_characteristics<T: CharDevOps>(
     _state: *mut c_void,
     c: *mut bindings::nk_char_dev_characteristics,
