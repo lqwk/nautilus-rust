7f66d8391b3ef03ccd014d7e8da836da4381d759
Ian Armstrong <armstrong.ian.m@gmail.com>
2023-05-24 15:06:34 -0500

    Broken WIP. Need registration

--- src/rust/src/virtio/virtio_gpu.rs
+++ src/rust/src/virtio/virtio_gpu.rs
@@ -1,19 +1,15 @@
 use crate::prelude::*;
 use crate::kernel::irq;
 
-use super::lock::IRQLock;
 
-mod virtio_gpu;
-
-pub struct 
 
 use crate::kernel::bindings;
 use crate::kernel::utils::to_c_string;
 use crate::prelude::*;
-use super::lock::IRQLock;
+// use super::lock::IRQLock;
 
 use core::{
-    ffi::{c_int, c_void, c_ushort},
+    ffi::{c_int, c_void, c_ushort,},
     intrinsics::write_bytes,
     ptr::null_mut,
 };
@@ -39,22 +35,183 @@ use alloc::{borrow::ToOwned, string::String, sync::Arc};
 
 // }
 
-pub struct NkGpuDev {
-    dev: *mut bindings::nk_gpu_dev,
-    name: String,
+pub struct GpuDevMode {
+    width: u32,
+    height: u32,
+    channel_offset: [u32; 4],
+    flags: u32,
+    mouse_cursor_width: u32,
+    mouse_cursor_height: u32,
+    mode_data: c_void,
 }
 
-impl NkGpuDev {
-
+pub enum VideoModes {
+    ChannelOffsetRed,
+    ChannelOffsetGreen,
+    ChannelOffsetBlue,
+    ChannelOffsetAlpha,
+    ChannelOffsetText,
+    ChannelOffsetAttr,
+    HasClipping,
+    HasClippingRegion,
+    HasMouseCursor,
 }
 
-impl Drop for NkGpuDev {
-    fn drop(&mut self) {
-        if let Some(ptr: &mut nk_gpu_dev) = unsafe { self.dev.as_mut() } {
-            unsafe {
-                let _ = Arc::from_raw(prt.dev.)
-                bindings::nk_gpu_dev_unregister(ptr);
-            }
+impl core::convert::From<VideoModes> for c_int {
+    fn from(mode: VideoModes) -> Self {
+        match mode {
+            VideoModes::ChannelOffsetRed => 0,
+            VideoModes::ChannelOffsetGreen => 1,
+            VideoModes::ChannelOffsetBlue => 2,
+            VideoModes::ChannelOffsetAlpha => 3,
+            VideoModes::ChannelOffsetText => 0,
+            VideoModes::ChannelOffsetAttr => 1,
+            VideoModes::HasClipping => 0x1,
+            VideoModes::HasClippingRegion => 0x2,
+            VideoModes::HasMouseCursor => 0x100,
         }
+    }
+}
+
+pub enum PassFail<T = ()> {
+    Ok(T),
+    Err,
+}
+
+impl<T> core::convert::From<PassFail<T>> for c_int {
+    fn from (pf: PassFail<T>) -> Self {
+        match pf {
+            PassFail::Ok(_) => 0,
+            PassFail::Err => -1,
+        }
+    }
+}
+
+pub trait GpuDev {
+    type State: Send + Sync;
+
+    fn get_available_modes(
+        state: &Self::State, 
+        // modes: Vec<GpuDevMode>,
+        // num_modes: &u32
+    ) -> PassFail;
+
+    fn set_mode(
+        state: &Self::State, 
+        // mode: *mut GpuDevMode
+    ) -> PassFail;
+
+    fn get_mode(
+        state: &Self::State, 
+        // mode: *mut GpuDevMode
+    ) -> PassFail;
+
+
+    // All should be async but not supported in trait impl
+    fn flush(state: &Self::State) -> PassFail;
+
+    fn text_set_char(
+        state: &Self::State,
+        // location: &bindings::nk_gpu_dev_coordinate_t,
+        // val: &bindings::nk_gpu_dev_char_t,
+    ) -> PassFail;
+
+    fn text_set_cursor(
+        state: &Self::State,
+        // location: &bindings::nk_gpu_dev_coordinate_t,
+        // flags: &u32/
+    ) -> PassFail;
+
+    fn graphics_set_clipping_box(
+        state: &Self::State,
+        // box_: &bindings::nk_gpu_dev_box_t,
+    ) -> PassFail;
+    
+    fn graphics_set_clipping_region(
+        state: &Self::State,
+        // region: &bindings::nk_gpu_dev_region_t,
+    ) -> PassFail;
+    
+    fn graphics_draw_pixel (
+        state: &Self::State,
+        // start: &bindings::nk_gpu_dev_coordinate_t,
+        // end: &bindings::nk_gpu_dev_coordinate_t,
+        // pixel: &bindings::nk_gpu_dev_pixel_t,
+    ) -> PassFail;
+
+    fn graphics_draw_line(
+        state: &Self::State,
+        // start: &bindings::nk_gpu_dev_coordinate_t,
+        // end: &bindings::nk_gpu_dev_coordinate_t,
+        // pixel: &bindings::nk_gpu_dev_pixel_t,
+    ) -> PassFail;
+
+    fn graphics_draw_poly(
+        state: &Self::State,
+        // coord_list: &bindings::nk_gpu_dev_coordinate_t,
+        // count: u32,
+        // pixel: &bindings::nk_gpu_dev_pixel_t,
+    ) -> PassFail;
+
+    fn graphics_fill_box_with_pixel(
+        state: &Self::State,
+        // _box: &bindings::nk_gpu_dev_box_t,
+        // pixel: &bindings::nk_gpu_dev_pixel_t,
+        // op: bindings::nk_gpu_dev_bit_blit_op_t,
+    ) -> PassFail;
+
+    fn graphics_fill_box_with_bitmap(
+        state: &Self::State,
+        // _box: &bindings::nk_gpu_dev_box_t,
+        // bitmap: &bindings::nk_gpu_dev_bitmap_t,
+        // op: &bindings::nk_gpu_dev_bit_blit_op_t,
+    ) -> PassFail;
+
+    fn graphics_copy_box(
+        state: &Self::State,
+        // source_box: &bindings::nk_gpu_dev_box_t,
+        // dest_box: &bindings::nk_gpu_dev_box_t, 
+        // op: &bindings::nk_gpu_dev_bit_blit_op_t,
+    ) -> PassFail;
+
+    fn graphics_draw_text(
+        state: &Self::State,
+        // location: &bindings::nk_gpu_dev_coordinate_t,
+        // font: &bindings::nk_gpu_dev_font_t,
+        // string: c_string,
+    ) -> PassFail;
+
+    fn graphics_set_cursor_bitmap (
+        state: &Self::State,
+        // bitmap: &bindings::nk_gpu_dev_bitmap_t,
+    ) -> PassFail;
+
+    fn graphics_set_cursor(
+        state: &Self::State,
+        // location: &bindings:nk_gpu_dev_coordinate_t,
+    ) -> PassFail;
+
+}
+
+pub struct Registration<G: GpuDev>;
+
+impl<G: GpuDev>Registration<G> {
+    unsafe extern "C" fn get_available_modes(
+        raw_state:c_void, 
+        // modes: Vec<GpuDevMode>,
+        num_modes: *mut u32
+    ) -> c_int {
+        let state = unsafe { (raw_state as *const G::State).as_ref() }.unwrap();
+
+        if num_modes < 2 {
+            PassFail::Err.into()
+        }
+    }
+
+    unsafe extern "C" fn set_mode(
+        raw_state: c_void, 
+        mode: *mut bindings::nk_gpu_dev_video_mode_t,
+    ) -> c_int {
+        let state = unsafe { (raw_state as *const G::State).as_ref() }.unwrap();
     }
 }
