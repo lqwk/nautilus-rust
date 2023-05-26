51860db7f60ad444f75976f8b43c8e98c07f6324
ubuntu <matthewsinclair2024@u.northwestern.edu>
2023-05-26 16:39:16 -0500

    Clean up thread.rs

--- src/rust/src/kernel/thread.rs
+++ src/rust/src/kernel/thread.rs
@@ -1,9 +1,9 @@
 //! Thread Kernel Module
 //!
-//! This module provides a safe and idiomatic Rust interface for managing AeroKernel threads.
+//! This module provides a safe and idiomatic Rust interface for managing Nautilus threads.
 //!
-//! The main struct provided by this module is `Thread`. This struct acts as a handle for an
-//! AeroKernel thread, and provides methods for creating and controlling threads.
+//! The main struct provided by this module is `Thread`. This struct acts as a handle for a
+//! Nautilus thread, and provides methods for creating and controlling threads.
 //!
 //! # Example
 //!
@@ -21,14 +21,16 @@
 //! calling FFI functions, or manipulating the thread of execution. The functions in this module
 //! carefully uphold the necessary invariants to ensure memory safety, but it's important to
 //! understand the underlying model when using this API.
-use super::bindings;
-extern crate cstr_core;
+use alloc::ffi::CString;
 use core::ffi::c_void;
+
+use crate::kernel::bindings;
+use crate::kernel::error::{Result, ResultExt};
 
 pub type ThreadId = *mut c_void;
 pub type ThreadFun = unsafe extern "C" fn(*mut c_void, *mut *mut c_void);
 
-/// A Rust-friendly wrapper for an AeroKernel thread.
+/// A Rust-friendly wrapper for a Nautilus thread.
 ///
 /// This struct contains the thread ID as well as the function,
 /// input, and output for the thread. It also includes information
@@ -43,15 +45,16 @@ pub struct Thread {
     id: ThreadId,
 }
 
+#[repr(u32)]
 pub enum ThreadStackSize {
-    Default = bindings::TSTACK_DEFAULT as isize,
-    _4KB = bindings::TSTACK_4KB as isize,
-    _1MB = bindings::TSTACK_1MB as isize,
-    _2MB = bindings::TSTACK_2MB as isize,
+    Default = bindings::TSTACK_DEFAULT,
+    _4KB = bindings::TSTACK_4KB,
+    _1MB = bindings::TSTACK_1MB,
+    _2MB = bindings::TSTACK_2MB,
 }
 
 impl Thread {
-    /// Create a new AeroKernel thread, but do not start it.
+    /// Create a new Nautilus thread, but do not start it.
     ///
     /// # Arguments
     ///
@@ -86,7 +89,7 @@ impl Thread {
         is_detached: bool,
         stack_size: ThreadStackSize,
         bound_cpu: i32,
-    ) -> Result<Self, i32> {
+    ) -> Result<Self> {
         let mut tid = core::ptr::null_mut();
         let mut is_detached_u8: u8;
         if is_detached {
@@ -94,19 +97,25 @@ impl Thread {
         } else {
             is_detached_u8 = 0;
         }
-        
-        // SAFETY: `nk_thread_create` is a C function that expects valid function pointers, input pointer, output pointer
-        // and stack size for successful thread creation. If successful, it returns a valid pointer in 'tid' that can be used in
+
+        // SAFETY: `nk_thread_create` is a C function that expects
+        // valid function pointers, input pointer, output pointer
+        // and stack size for successful thread creation. If successful,
+        // it returns a valid pointer in 'tid' that can be used in
         // other thread operations.
         let ret = unsafe {
-            bindings::nk_thread_create(fun, input, output, is_detached_u8, stack_size as u64, &mut tid, bound_cpu)
+            bindings::nk_thread_create(
+                fun,
+                input,
+                output,
+                is_detached_u8,
+                stack_size as u64,
+                &mut tid,
+                bound_cpu,
+            )
         };
-        
-        if ret == 0 {
-            Ok(Self { id: tid })
-        } else {
-            Err(ret)
-        }
+
+        Result::from_error_code(ret).map(|_| Self { id: tid })
     }
 
     /// Runs the thread.
@@ -129,16 +138,13 @@ impl Thread {
     /// let result = thread.run();
     /// assert!(result.is_ok());
     /// ```
-    pub fn run(&self) -> Result<(), i32> {
-        // SAFETY: `nk_thread_run` is a C function that expects a valid thread id that was returned from a successful
-        // thread creation. If successful, it sets the thread running and returns 0.  
+    pub fn run(&self) -> Result {
+        // SAFETY: `nk_thread_run` is a C function that expects a valid
+        // thread id that was returned from a successful thread creation.
+        // If successful, it sets the thread running and returns 0.
         let ret = unsafe { bindings::nk_thread_run(self.id) };
-        
-        if ret == 0 {
-            Ok(())
-        } else {
-            Err(ret)
-        }
+
+        Result::from_error_code(ret)
     }
 
     /// Starts and runs a new thread.
@@ -177,7 +183,7 @@ impl Thread {
         is_detached: bool,
         stack_size: ThreadStackSize,
         bound_cpu: i32,
-    ) -> Result<Self, i32> {
+    ) -> Result<Self> {
         let mut tid = core::ptr::null_mut();
         let mut is_detached_u8: u8;
         if is_detached {
@@ -185,19 +191,25 @@ impl Thread {
         } else {
             is_detached_u8 = 0;
         }
-    
-        // SAFETY: `nk_thread_start` is a C function that expects valid function pointers, input pointer, output pointer
-        // and stack size for successful thread creation and running. If successful, it returns a valid pointer in 'tid' that can be used in
-        // other thread operations.
+
+        // SAFETY: `nk_thread_start` is a C function that expects valid
+        // function pointers, input pointer, output pointer and stack
+        // size for successful thread creation and running. If successful,
+        // it returns a valid pointer in 'tid' that can be used in other
+        // thread operations.
         let ret = unsafe {
-            bindings::nk_thread_start(fun, input, output, is_detached_u8, stack_size as u64, &mut tid, bound_cpu)
+            bindings::nk_thread_start(
+                fun,
+                input,
+                output,
+                is_detached_u8,
+                stack_size as u64,
+                &mut tid,
+                bound_cpu,
+            )
         };
-        
-        match ret {
-            0 => Ok(Self { id: tid }),
-            -1 => Err(-1),  // Could not create thread. handle the -1 case specifically if needed
-            _ => Err(ret),
-        }
+
+        Result::from_error_code(ret).map(|_| Self { id: tid })
     }
 
     /// Fork the current thread.
@@ -228,7 +240,7 @@ impl Thread {
     /// This function is unsafe because it calls the C function `nk_set_thread_output`.
     /// The safety of this function depends on the correct implementation of the C library.
     pub fn set_output(&self, result: *mut c_void) {
-        // SAFETY: `nk_set_thread_output` is a C function that expects a valid pointer for the result value. 
+        // SAFETY: `nk_set_thread_output` is a C function that expects a valid pointer for the result value.
         // It sets the output for the thread and does not return a value.
         unsafe { bindings::nk_set_thread_output(result) }
     }
@@ -257,16 +269,12 @@ impl Thread {
     /// let result = thread.join();
     /// assert!(result.is_ok());
     /// ```
-    pub fn join(&self, output: *mut *mut c_void) -> Result<(), i32> {
+    pub fn join(&self, output: *mut *mut c_void) -> Result {
         // SAFETY: `nk_join` is a C function that expects a valid thread id that was returned from a successful
         // thread creation. It also expects a valid pointer for storing thread output.
         let ret = unsafe { bindings::nk_join(self.id, output) };
-        
-        if ret == 0 {
-            Ok(())
-        } else {
-            Err(ret)
-        }
+
+        Result::from_error_code(ret)
     }
 
     /// Destroys the thread.
@@ -282,7 +290,7 @@ impl Thread {
     }
 
     /// Exits the thread.
-    /// 
+    ///
     /// Called explictly or implicitly when a thread exits.
     ///
     /// # Arguments
@@ -294,11 +302,11 @@ impl Thread {
     /// This function is unsafe because it calls the C function `nk_thread_exit`.
     /// The safety of this function depends on the correct implementation of the C library.
     pub fn exit(&self, retval: *mut c_void) {
-        // SAFETY: `nk_thread_exit` is a C function that expects a valid pointer for the return value. 
+        // SAFETY: `nk_thread_exit` is a C function that expects a valid pointer for the return value.
         // It causes the calling thread to terminate and does not return a value.
         unsafe { bindings::nk_thread_exit(retval) }
     }
-    
+
     /// Causes the thread to yield execution to another thread that is ready to run.
     ///
     /// # Safety
@@ -335,20 +343,16 @@ impl Thread {
     /// let result = thread.name("MyThread");
     /// assert!(result.is_ok());
     /// ```
-    pub fn name(&self, name: &str) -> Result<(), i32> {
-        let cstr = cstr_core::CString::new(name).unwrap();
-    
-        // SAFETY: `nk_thread_name` is a C function that expects a valid thread id and a valid null-terminated string pointer for the thread name.
-        // If successful, it sets the thread name and returns 0. 
+    pub fn name(&self, name: &str) -> Result {
+        let cstr = CString::new(name).unwrap();
+
+        // SAFETY: `nk_thread_name` is a C function that expects a valid
+        // thread id and a valid null-terminated string pointer for the thread name.
+        // If successful, it sets the thread name and returns 0.
         let ret = unsafe { bindings::nk_thread_name(self.id, cstr.as_ptr() as *mut i8) };
-    
-        if ret == 0 {
-            Ok(())
-        } else {
-            Err(ret)
-        }
+
+        Result::from_error_code(ret)
     }
-    
 
     /// Gets the thread id of the current thread.
     ///
@@ -360,11 +364,20 @@ impl Thread {
     ///
     /// This function is unsafe because it calls the C function `nk_get_tid`.
     /// The safety of this function depends on the correct implementation of the C library.
-    #[cfg(not(feature = "LEGION"))]
     pub fn get_tid() -> Self {
+        // `bindings::nk_get_tid` will not exist if the LEGION runtime has been built
+        // into the kernel. We don't want everything to fail to compile if LEGION is built, 
+        // so we introduce a runtime panic if this function if called with `__LEGION__` defined.
+        //
+        // Ideally, we would compile our own `Thread::get_tid` only if `__LEGION__` is
+        // not defined (like the C code does), but `#[cfg(not(accessible(...))]` is not
+        // a thing yet.
+        #[cfg_accessible(crate::kernel::bindings::__LEGION__)]
+        panic!("Thread::get_tid cannot be used with the LEGION runtime.");
+
         // SAFETY: `nk_get_tid` is a C function that gets the current thread id.
         let tid = unsafe { bindings::nk_get_tid() };
-        
+
         Self { id: tid }
     }
 
@@ -381,7 +394,9 @@ impl Thread {
     pub fn get_parent_tid() -> Self {
         // SAFETY: `nk_get_parent_tid` is a C function that gets the parent thread id.
         let tid = unsafe { bindings::nk_get_parent_tid() };
-        
+
         Self { id: tid }
     }
 }
+
+
