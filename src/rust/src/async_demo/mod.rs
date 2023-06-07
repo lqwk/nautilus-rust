e77c69b8742dfb4b99183ce6f29f43b13fe8a06d
Ian Armstrong <armstrong.ian.m@gmail.com>
2023-06-06 23:30:55 -0500

    updated to spawn 100 random tasks that return after [0.0, 10.0] second delay

--- src/rust/src/async_demo/mod.rs
+++ src/rust/src/async_demo/mod.rs
@@ -2,53 +2,48 @@ use crate::prelude::*;
 use crate::kernel::task::{Task, executor::Executor};
 use crate::kernel::task::utils::yield_now;
 use crate::kernel::timer;
-use crate::kernel::thread;
+use rand_core::{RngCore, SeedableRng};
+use rand_xorshift::XorShiftRng;
 
 make_logging_macros!("async");
 
-
 fn kernel_main() {
     let mut executor = Executor::new();
-    executor.spawn(Task::new(example_task_1()));
-    executor.spawn(Task::new(example_task_2()));
-    executor.spawn(Task::new(example_task_3()));
+
+    vc_println!("Spawning 100 async tasks...");
+
+    // Spawn 100 async tasks
+    for i in 0..99 {
+        let duration_secs = generate_random_float(i);
+        executor.spawn(Task::new(async_task(i, duration_secs)));
+    }
+
+
     executor.run(true); // This will now return once all tasks are done
 }
 
-async fn async_number() -> u32 {
-    let ns = 5 * 1_000_000_000; // 5 seconds in nanoseconds
-    let start = timer::get_realtime(); // get the current time
+// Generate a random floating point number
+// Hack to get around the fact rand doesn't work in no_std
+fn generate_random_float(seed: u64) -> f64 {
+    let mut rng = XorShiftRng::seed_from_u64(seed);
+    let float_part: f64 = rng.next_u32() as f64 / u32::MAX as f64;  // generates a random float between 0 and 1
+    float_part * 10.0  // scales it to a range of 0.0 to 10.0
+}
+
+async fn async_number(task_num: u64, duration: f64) -> u32 {
+    let ns = (duration * 1_000_000_000.0) as u64; // converts duration to nanoseconds
+    let start = timer::get_realtime();
     while timer::get_realtime() < start + ns {
-        yield_now().await; // yield control to allow other tasks to run
+        yield_now().await;
     }
-    42
+    task_num as u32 // return the simulated result of the async task
 }
 
-async fn example_task_1() {
-    vc_println!("Hello from example task 1!\nI'm a blocking task to show the timer!\nWaiting 3 seconds...");
-    // `thread::sleep` is a blocking function. So this task cannot be interrupted during this
-    // sleep, unlike above, where we yield during the sleep.
-    //
-    // It would be nice to have an `async_sleep` function.
-    thread::sleep(core::time::Duration::from_secs(3));
-    vc_println!("Done sleeping!");
-    vc_println!("blocking number 1: {}", 8086);
-}
-
-async fn example_task_2() {
-    vc_println!("Hello from example task 2! Waiting 5 seconds...");
-    let number = async_number().await;
-    vc_println!("async number 2: {}", number);
-}
-
-async fn async_number_3() -> u32 {
-    69
-}
-
-async fn example_task_3() {
-    vc_println!("Hello from example task 3! Number will return now...");
-    let number = async_number_3().await;
-    vc_println!("async number 3: {}", number);
+// This is the async task that will be spawned
+async fn async_task(task_num: u64, duration: f64) {
+    vc_println!("Hello from async task {}! Waiting {} seconds...", task_num, duration);
+    let num = async_number(task_num, duration).await;
+    vc_println!("Async task {} done!", num);
 }
 
 register_shell_command!("rust_async", "rust_async", |_| {
