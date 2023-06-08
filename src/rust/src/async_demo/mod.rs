0b80723d03407e67829b0999efbc36511c5d2675
ubuntu <matthewsinclair2024@u.northwestern.edu>
2023-06-08 16:50:11 -0500

    Remove dependencies on rand crates

--- src/rust/src/async_demo/mod.rs
+++ src/rust/src/async_demo/mod.rs
@@ -2,10 +2,9 @@ use crate::prelude::*;
 use crate::kernel::task::{Task, executor::Executor};
 use crate::kernel::task::utils::yield_now;
 use crate::kernel::timer;
-use rand_core::{RngCore, SeedableRng};
-use rand_xorshift::XorShiftRng;
+use core::time::Duration;
 
-make_logging_macros!("async");
+make_logging_macros!("async_demo");
 
 fn kernel_main() {
     let mut executor = Executor::new();
@@ -14,24 +13,28 @@ fn kernel_main() {
 
     // Spawn 100 async tasks
     for i in 0..99 {
-        let duration_secs = generate_random_float(i);
-        executor.spawn(Task::new(async_task(i, duration_secs)));
+        let dur = Duration::from_secs_f32(generate_random_f32(0.0, 10.0));
+        executor.spawn(Task::new(async_task(i, dur)));
     }
 
 
     executor.run(true); // This will now return once all tasks are done
 }
 
-// Generate a random floating point number
-// Hack to get around the fact rand doesn't work in no_std
-fn generate_random_float(seed: u64) -> f64 {
-    let mut rng = XorShiftRng::seed_from_u64(seed);
-    let float_part: f64 = rng.next_u32() as f64 / u32::MAX as f64;  // generates a random float between 0 and 1
-    float_part * 10.0  // scales it to a range of 0.0 to 10.0
+/// Generate a random floating point number
+///
+/// Eventually this should be brought into a `rand` module
+/// (with an API similar to the standard lib probably).
+fn generate_random_f32(min: f32, max: f32) -> f32 {
+    // SAFETY: FFI call.
+    let scale = unsafe { 
+        (super::kernel::bindings::rand() as f32) / (super::kernel::bindings::RAND_MAX as f32)
+    };
+    min + scale * ( max - min )
 }
 
-async fn async_number(task_num: u64, duration: f64) -> u32 {
-    let ns = (duration * 1_000_000_000.0) as u64; // converts duration to nanoseconds
+async fn async_number(task_num: u64, dur: Duration) -> u32 {
+    let ns = dur.as_nanos() as u64;
     let start = timer::get_realtime();
     while timer::get_realtime() < start + ns {
         yield_now().await;
@@ -40,9 +43,9 @@ async fn async_number(task_num: u64, duration: f64) -> u32 {
 }
 
 // This is the async task that will be spawned
-async fn async_task(task_num: u64, duration: f64) {
-    vc_println!("Hello from async task {}! Waiting {} seconds...", task_num, duration);
-    let num = async_number(task_num, duration).await;
+async fn async_task(task_num: u64, dur: Duration) {
+    vc_println!("Hello from async task {}! Waiting for {} secs...", task_num, dur.as_secs_f32());
+    let num = async_number(task_num, dur).await;
     vc_println!("Async task {} done!", num);
 }
 
